558759b9a74f2074bf4fc7700c810db49479f7e4
Sanmeet Wakchaure <wakchauresanmeet3@gmail.com>
2022-12-19 14:45:11 +0530

    user created and saved to db using POST request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 // define the home page route
-router.get('/', (req, res) => {
-    obj = {
-        myName:"Sanmeet",
-        myAge:21
-    }
-  res.send(obj);
+//Create a user using POST /api/auth ; Doesn't require Authentication
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
